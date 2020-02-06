4a904d5937d5e7237226a7970bb16749ab7e290c
ngocpt <tuanngocptn@gmail.com>
2020-02-07 02:04:18 +0700

    Updates

---
+++ precache-manifest.82dd82004865cb891bfe058519f1035b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2a8807fb170aafbd18a3e24daa026b",
+    "url": "/xosovn/index.html"
+  },
+  {
+    "revision": "0939d969849e80129dbb",
+    "url": "/xosovn/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3ba91ecba268362e37e9",
+    "url": "/xosovn/static/js/2.689c2083.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/xosovn/static/js/2.689c2083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0939d969849e80129dbb",
+    "url": "/xosovn/static/js/main.9e417240.chunk.js"
+  },
+  {
+    "revision": "22c45033b339f7e670f7",
+    "url": "/xosovn/static/js/runtime-main.ad0b4d64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/xosovn/static/media/logo.5d5d9eef.svg"
+  }
+]);
