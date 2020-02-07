c386fe6452a8b71ebc6cced816cc3a28441413b0
ngocpt <tuanngocptn@gmail.com>
2020-02-07 14:45:33 +0700

    refactor code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './modules/App/pages/App';
+import * as serviceWorker from './modules/App/workers/serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
